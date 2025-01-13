d0fc40fd4ad24680908c5c0f64d87e5d76d2928e
Ishan23456 <ishannaeem1@gmail.com>
2025-01-13 12:51:59 +0000

    fix(09-rotate-angles): fixed rotated angles

--- 09. rotate-angle/solution.js
+++ 09. rotate-angle/solution.js
@@ -1,7 +1,10 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
-  let newAngle = initialAngle + (rotation % 360);
-
+  let newAngle = (initialAngle + rotation) % 360;
+  console.log(newAngle)
+  if (newAngle < 0) {
+    return newAngle + 360;
+  }
   return newAngle;
 }
 
